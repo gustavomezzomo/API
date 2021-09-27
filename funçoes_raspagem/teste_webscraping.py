64438d72d566953d9f55442d345ec63ba74f0a4c
Gustavo Mezzomo <gkmezzomo@gmail.com>
2021-09-27 08:48:04 -0300

    resolve problemas arquivo txt

--- funçoes_raspagem/teste_webscraping.py
+++ funçoes_raspagem/teste_webscraping.py
@@ -1,10 +1,6 @@
 from typing import List, Text
 from bs4 import BeautifulSoup
 import requests
-from typing import Container
-from cursos import *
-from nomes import nome_equipes
-import os
 
 def raspar_2020_1():
 
@@ -16,7 +12,8 @@ def raspar_2020_1():
 
     var2 = 0
     lista_link = []
-    lista_grupo = []
+    lista_nomes = []
+    lista_turma = []
     var3 = 0
 
     while var2 <= 10:
@@ -24,19 +21,31 @@ def raspar_2020_1():
         var = 'https://fatecsjc-prd.azurewebsites.net/api/2020-1/' +link[var2]
         lista_link.append(var)
         var2 = var2 + 1
-
+        
     while var3 <=10:
         source = requests.get(lista_link[var3]).text
         soup = BeautifulSoup(source, 'lxml')
-        text = [i for i in soup.find_all('a', text=True)]
+        turma = [i for i in soup.find_all('h3')]
+        for i in turma [0:]:
+            nome = i.text.strip()
+        lista_turma.append(nome)
+        text = [i for i in soup.find_all('a', href=True)]
+        for x in text [0:]:
+            lista_nomes.append(x.text.strip())
         link = [i['href'] for i in soup.find_all('a', href=True)]
-        lista_grupo.append(text)
-        lista_grupo.append(link)
+        while True:
+            try:
+                lista_turma.append(lista_nomes.pop(0))
+                lista_turma.append(link.pop(0))
+            except IndexError:
+                break
         var3 = var3 + 1
 
+
+
     textfile = open("2020-1_file.txt", "w")
-    for element in lista_grupo:
-        textfile.write(element + "\n")
+    for element in lista_turma:
+            textfile.write(element + "\n")
     textfile.close()
 
 def raspar_2020_2():
--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,7 @@
 from typing import List, Text
 from bs4 import BeautifulSoup
 import requests
+from functools import *
 
 source = requests.get('https://fatecsjc-prd.azurewebsites.net/api/2020-1/turmas_2020-1.php').text
 
@@ -10,7 +11,8 @@ link = [i['href'] for i in soup.find_all('a', href=True)]
 
 var2 = 0
 lista_link = []
-lista_grupo = []
+lista_nomes = []
+lista_turma = []
 var3 = 0
 
 while var2 <= 10:
@@ -18,14 +20,29 @@ while var2 <= 10:
     var = 'https://fatecsjc-prd.azurewebsites.net/api/2020-1/' +link[var2]
     lista_link.append(var)
     var2 = var2 + 1
-
+    
 while var3 <=10:
     source = requests.get(lista_link[var3]).text
     soup = BeautifulSoup(source, 'lxml')
-    text = [i for i in soup.find_all('a', text=True)]
+    turma = [i for i in soup.find_all('h3')]
+    for i in turma [0:]:
+        nome = i.text.strip()
+    lista_turma.append(nome)
+    text = [i for i in soup.find_all('a', href=True)]
+    for x in text [0:]:
+        lista_nomes.append(x.text.strip())
     link = [i['href'] for i in soup.find_all('a', href=True)]
-    lista_grupo.append(text)
-    lista_grupo.append(link)
+    while True:
+        try:
+            lista_turma.append(lista_nomes.pop(0))
+            lista_turma.append(link.pop(0))
+        except IndexError:
+            break
     var3 = var3 + 1
 
-print(lista_grupo)
+
+
+textfile = open("2020-1_file.txt", "w")
+for element in lista_turma:
+        textfile.write(element + "\n")
+textfile.close()
